610316af3f258fffda1f3fdd404c14df22a3db34
LIGHTCLINTON <liclintght@gmail.com>
2025-07-28 10:38:59 +0300

    Add all images and assets

--- app.js
+++ app.js
@@ -1,13 +1,9 @@
-const menu = document.querySelector('#mobile-menu');
-const menuLinks = document.querySelector('.navbar__menu');
-const navLogo = document.querySelector('#navbar__logo');
-const body = document.querySelector('body');
+document.addEventListener("DOMContentLoaded", function() {
+    const menuToggle = document.getElementById("mobile-menu");
+    const navbarMenu = document.querySelector(".navbar__menu");
 
-// Display Mobile Menu
-const mobileMenu = () => {
-    menu.classList.toggle('is-active');
-    menuLinks.classList.toggle('active');
-    body.classList.toggle('no-scroll');
-}
-
-menu.addEventListener('click', mobileMenu);
+    menuToggle.addEventListener("click", function() {
+        navbarMenu.classList.toggle("active");
+        menuToggle.classList.toggle("is-active");
+    });
+});
